ed19f0aece9bbd64717219c555b4ed5bd2530396
Steven Drury <drury.steven@gmail.com>
2016-06-12 22:15:11 +1000

    Added recursive to cp

--- distribute.sh
+++ distribute.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Use this file to move the Git repo files to the correct areas and change their permissions so you can use them!
-# Should be run as su, otherwise the chmod will fail. 
+# Should be run as su, otherwise the chmod will fail.
 
-cp * /var/www/html/
+cp * -R /var/www/html/
 cd /var/www/html
 chmod 777 *
