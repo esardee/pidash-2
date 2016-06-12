a0f136af9b2664e7e5fa31cf33eca252c6a23fd9
Steven Drury <drury.steven@gmail.com>
2016-06-12 22:28:00 +1000

    Added content for file

--- scripts/softboot.sh
+++ scripts/softboot.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo $(date) "- This is a softboot!" > /Users/Steve/Desktop/php.txt
+# Find the PID for iceweasel and kill it
+pid_iceweasel="$(pgrep -f iceweasel)"
+kill $pid_iceweasel
+
+# Reload the new file...
+# [this might not work]
+sh /home/pi/fullsreen.#!/bin/sh
