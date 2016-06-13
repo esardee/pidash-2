35a6524610452ed2547cdced282988db8f5af8ad
Steven Drury <drury.steven@gmail.com>
2016-06-13 20:20:49 +1000

    Commenting out unused code.

--- index.js
+++ index.js
@@ -31,30 +31,30 @@ $('#reboot').click(function(){
 // --- Everything below here is TEST.
 // ------------------------------------------------------------
 
-$('#create').submit(function() { // catch the forms submit event
-  $.ajax({ // create the AJAX call
-    data: $(this).serialize(), // get the form data
-    type: $(this).attr('method'), // GET or POST
-    url: $(this).attr('action'), // the file to call on submit
-    success: function(response) { // on success
-      $('#created').html(response); // update the DIV
-    }
-  });
-  return false; // cancel the original event to prevent the form submitting
-});
+// $('#create').submit(function() { // catch the forms submit event
+//   $.ajax({ // create the AJAX call
+//     data: $(this).serialize(), // get the form data
+//     type: $(this).attr('method'), // GET or POST
+//     url: $(this).attr('action'), // the file to call on submit
+//     success: function(response) { // on success
+//       $('#created').html(response); // update the DIV
+//     }
+//   });
+//   return false; // cancel the original event to prevent the form submitting
+// });
 
 // ------------------------------------------------------------
 // --- Everything below here is SAVED!
 // ------------------------------------------------------------
 
-$('#urlForm').submit(function() {
-  $.ajax({
-    data: $(this).serialize(),
-    type: $(this).attr('method'),
-    url: $(this).attr('action'),
-    success: function(response) {
-      $('#success').html(response);
-    }
-  });
-  return false;
-});
+// $('#urlForm').submit(function() {
+//   $.ajax({
+//     data: $(this).serialize(),
+//     type: $(this).attr('method'),
+//     url: $(this).attr('action'),
+//     success: function(response) {
+//       $('#success').html(response);
+//     }
+//   });
+//   return false;
+// });
