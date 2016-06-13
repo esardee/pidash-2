2d2c94450fb59231ab48228983d120879e2e4409
Steven Drury <drury.steven@gmail.com>
2016-06-13 20:10:51 +1000

    fullscreen.sh troubleshooting

--- scripts/fullscreen.sh
+++ scripts/fullscreen.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-if ! pgrep "iceweasel" > /dev/null
-then
+# if ! pgrep "iceweasel" > /dev/null
+# then
+    # kill all existing iceweasel instances anyway
+    pid_iceweasel="$(pgrep -f iceweasel)"
+    kill $pid_iceweasel
+    sleep 1s
+
+    # Restart the browser
     unclutter &
     matchbox-window-manager &
     iceweasel https://www.g123123oo222gle.com --display=:1 &
     sleep 10s
-fi
+# fi
--- scripts/update.php
+++ scripts/update.php
@@ -15,12 +15,12 @@ if (!empty($url)) {
 
     // $txt = 'unclutter &' . PHP_EOL . 'matchbox-window-manager &' . PHP_EOL . 'iceweasel ' . $url . ' --display=:1 &' . PHP_EOL . 'sleep 10s' . PHP_EOL ;
 
-    $txt = 'if ! pgrep "iceweasel" > /dev/null' . PHP_EOL . 'then' . PHP_EOL . 'unclutter &' . PHP_EOL . 'matchbox-window-manager &' . PHP_EOL . 'iceweasel ' . $url . ' --display=:1 &' . PHP_EOL . 'sleep 10s' . PHP_EOL . 'fi' . PHP_EOL ;
+    $txt = '# kill all existing iceweasel instances anyway' . PHP_EOL . 'pid_iceweasel="$(pgrep -f iceweasel)"' . PHP_EOL . 'kill $pid_iceweasel' . PHP_EOL . 'sleep 1s' . PHP_EOL . PHP_EOL . '# Restart the browser' . PHP_EOL . 'unclutter &' . PHP_EOL . 'matchbox-window-manager &' . PHP_EOL . 'iceweasel ' . $url . ' --display=:1 &' . PHP_EOL . 'sleep 10s' . PHP_EOL ;
 
     fwrite($newConfig, $txt) ;
     fclose($newConfig) ;
 
-    $kill = shell_exec('pid_iceweasel="$(pgrep -f iceweasel)" && kill $pid_iceweasel && sleep 1s');
+    // $kill = shell_exec('pid_iceweasel="$(pgrep -f iceweasel)" && kill $pid_iceweasel && sleep 1s');
     $reload = shell_exec('sh /home/pi/fullscreen.sh');
 
   } else {
